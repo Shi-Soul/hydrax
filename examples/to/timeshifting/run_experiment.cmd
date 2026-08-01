b46b5c986d143024b2694b2fc0aa352687f63ea8
Shi-Soul <993520026@qq.com>
2026-08-01 17:30:26 +0800

    Make experiment command self-bootstrapping via uv

--- examples/to/timeshifting/run_experiment.cmd
+++ examples/to/timeshifting/run_experiment.cmd
@@ -6,5 +6,8 @@ cd "${repository_root}"
 export EQX_ON_ERROR=nan
 export MUJOCO_GL=egl
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
-exec .venv/bin/python -m examples.to.timeshifting.experiment "$@"
 
+if [[ -x .venv/bin/python ]]; then
+  exec .venv/bin/python -m examples.to.timeshifting.experiment "$@"
+fi
+exec uv run --frozen python -m examples.to.timeshifting.experiment "$@"
